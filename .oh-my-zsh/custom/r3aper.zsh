02eaf87fe801047c6858ef2bc49c95783f1a6a5b
Pierre FILSTROFF <pfilstroff@gmail.com>
2012-10-16 21:52:16 +0200

    Choosed gallois theme for oh my zsh and also removed right rvm prompt

--- .oh-my-zsh/custom/r3aper.zsh
+++ .oh-my-zsh/custom/r3aper.zsh
@@ -1,10 +1,10 @@
-ZSH_THEME="robbyrussell"
+ZSH_THEME="gallois"
 COMPLETION_WAITING_DOTS="true"
 
 # PLUGINS
 plugins=(git rails brew bundler cap gem github osx python rails3 rake rvm screen ssh-keygen svn rbenv)
 
-# EXPORTS
+# EXPORTS"
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:$PATH
 export PATH=/usr/local/sbin:/opt/local/bin:/usr/local/mysql/bin:$PATH
 
@@ -15,14 +15,3 @@ export GREP_COLOR='1;32'
 
 #RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-local rvm=''
-if which rvm-prompt &> /dev/null; then
-    rvm='%{$fg[green]%}‹$(rvm-prompt i v g)›%{$reset_color%}'
-else
-    if which rbenv &> /dev/null; then
-        rvm='%{$fg[green]%}‹$(rbenv version | sed -e "s/ (set.*$//")›%{$reset_color%}'
-    fi  
-fi
-
-RPROMPT="${rvm}"
