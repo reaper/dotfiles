1eddd98b41749bca5e05de65d128dcc499d88440
Pierre FILSTROFF <pfilstroff@gmail.com>
2012-12-05 11:08:25 +0100

    Fixed anoying slow git completion

--- .oh-my-zsh/custom/r3aper.zsh
+++ .oh-my-zsh/custom/r3aper.zsh
@@ -2,7 +2,7 @@ ZSH_THEME="gallois" #THEME
 COMPLETION_WAITING_DOTS="true"
 
 #ZSH PLUGINS
-plugins=(git rails brew bundler cap gem github osx python rails3 rake rvm screen ssh-keygen svn rbenv)
+plugins=(git rails brew bundler cap gem github osx python rails3 rake rvm screen ssh-keygen svn)
 
 #EXPORTS"
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:$PATH #COMMON
@@ -18,10 +18,15 @@ export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='1;32'
 
 #ALIAS
-alias qmake='qmake -spec macx-llvm'
+alias qmake='qmake -spec unsupported/macx-clang'
 
 #RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 
 #PYTHONBREW
 [[ -s $HOME/.pythonbrew/etc/bashrc ]] && source $HOME/.pythonbrew/etc/bashrc
+
+# Fix slow git completion
+__git_files () { 
+  _wanted files expl 'local files' _files
+}
