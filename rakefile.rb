e11c2024472fd58f996cea73eb984e10d1bae8ab
Pierre FILSTROFF <pfilstroff@gmail.com>
2013-01-18 15:40:45 +0100

    Add own ruby indent vim syntax

--- rakefile.rb
+++ rakefile.rb
@@ -12,107 +12,107 @@ task :default => [:install]
 
 desc "Copy files from dotfiles to home directory and build"
 task :install do
-    # Backup files
-    Rake::Task["backup"].invoke
-    # Replace files from dotfiles folder to home
-    Rake::Task["replace"].invoke
-    # Cleanup dotfiles backup folder 
-    Rake::Task["cleanup"].invoke
-    # Update vim plugins
-    Rake::Task["get_vim_plugins"].invoke
+  # Backup files
+  Rake::Task["backup"].invoke
+  # Replace files from dotfiles folder to home
+  Rake::Task["replace"].invoke
+  # Cleanup dotfiles backup folder 
+  Rake::Task["cleanup"].invoke
+  # Update vim plugins
+  Rake::Task["get_vim_plugins"].invoke
 end
 
 desc "Cleanup backup directory"
 task :cleanup do
-    
-    puts "\nCleanup ~/.dotfiles directory"
-    puts "------------------------------------------------"
-    
-    destination_dir = File.join(@home_path, @backup_folder)
-    
-    # Get folders list
-    folders = Dir.entries(destination_dir).reject{|entry| entry == "." || entry == ".."}.sort.reverse
-    
-    # Remove old folders and keep five lasts
-    if folders.size > 10
-        first_ten_folder = folders[0, 10]
-        folders_to_remove = folders - first_ten_folder
-        
-        for folder in folders_to_remove
-            folder_path = File.join(destination_dir, folder)
-            FileUtils.rm_rf(folder_path)
-            puts "Removed '#{folder_path}'"
-        end
-        puts "Done."
-    else
-        puts "Nothing to remove."
+
+  puts "\nCleanup ~/.dotfiles directory"
+  puts "------------------------------------------------"
+
+  destination_dir = File.join(@home_path, @backup_folder)
+
+  # Get folders list
+  folders = Dir.entries(destination_dir).reject{|entry| entry == "." || entry == ".."}.sort.reverse
+
+  # Remove old folders and keep five lasts
+  if folders.size > 10
+    first_ten_folder = folders[0, 10]
+    folders_to_remove = folders - first_ten_folder
+
+    for folder in folders_to_remove
+      folder_path = File.join(destination_dir, folder)
+      FileUtils.rm_rf(folder_path)
+      puts "Removed '#{folder_path}'"
     end
+    puts "Done."
+  else
+    puts "Nothing to remove."
+  end
 end
 
 desc "Backup dot files"
 task :backup do
-    
-    puts "\nBackup dot files in ~/.dotfiles directory"
-    puts "------------------------------------------------"
-    
-    timestamp = Time.now.getutc.strftime("%Y%m%d%H%M%S")
-    destination_dir = File.join(@home_path, @backup_folder, timestamp)
-    
-    FileUtils.mkdir_p(destination_dir, :verbose => true)
-    copy_files(@files, @home_path, destination_dir)
-    
-    puts "Done."
+
+  puts "\nBackup dot files in ~/.dotfiles directory"
+  puts "------------------------------------------------"
+
+  timestamp = Time.now.getutc.strftime("%Y%m%d%H%M%S")
+  destination_dir = File.join(@home_path, @backup_folder, timestamp)
+
+  FileUtils.mkdir_p(destination_dir, :verbose => true)
+  copy_files(@files, @home_path, destination_dir)
+
+  puts "Done."
 end
 
 desc "Replace files"
 task :replace do
-    
-    puts "\nReplace files to the home directory"
-    puts "------------------------------------------------"
-    
-    copy_files(@files, @file_dir_path, @home_path)
-    
-    puts "Done."
+
+  puts "\nReplace files to the home directory"
+  puts "------------------------------------------------"
+
+  copy_files(@files, @file_dir_path, @home_path)
+
+  puts "Done."
 end
 
 desc "Get vim plugins"
 task :get_vim_plugins do
-    
-    puts "\nGet vim plugins"
-    puts "------------------------------------------------"
-    
-    vim_path = File.join(@home_path, ".vim")
-    
-    FileUtils.mkdir_p(File.join(vim_path, "bundle"), :verbose => true)
-    ruby File.join(vim_path, "update_vim.rb")
-    
-    puts "Done."
+
+  puts "\nGet vim plugins"
+  puts "------------------------------------------------"
+
+  vim_path = File.join(@home_path, ".vim")
+
+  FileUtils.mkdir_p(File.join(vim_path, "bundle"), :verbose => true)
+  ruby File.join(vim_path, "update_vim.rb")
+
+  puts "Done."
 end
 
 # Copy files from a source dir to a destination dir
 def copy_files(files, source_dir, destination_dir)
-    
-    for file in files
-        file_path = File.join(source_dir, file) 
-        
-        if File.exist?(file_path)
-            if File.directory?(file_path)
-                
-                full_destination_folder = File.join(destination_dir, File.basename(file).prepend("."))
-                FileUtils.rm_r(full_destination_folder, :force => true, :verbose => true)
-                FileUtils.cp_r(file_path, full_destination_folder, :verbose => true)
-            else
-                dir_name = File.dirname(file)
 
-                if(dir_name.eql?("."))
-                        FileUtils.mkdir_p(destination_dir, :verbose => true)
-                        FileUtils.cp_r(file_path, File.join(destination_dir, File.basename(file).prepend(".")), :verbose => true)
-                else
-                        full_destination_folder = File.join(destination_dir, dir_name.prepend("."))
-                        FileUtils.mkdir_p(full_destination_folder, :verbose => true)
-                        FileUtils.cp_r(file_path, File.join(full_destination_folder, File.basename(file)), :verbose => true)
-                end
-            end
+  for file in files
+    file_path = File.join(source_dir, file) 
+
+    if File.exist?(file_path)
+      if File.directory?(file_path)
+
+        full_destination_folder = File.join(destination_dir, File.basename(file).prepend("."))
+        FileUtils.rm_r(full_destination_folder, :force => true, :verbose => true)
+        FileUtils.cp_r(file_path, full_destination_folder, :verbose => true)
+      else
+        dir_name = File.dirname(file)
+
+        if(dir_name.eql?("."))
+          FileUtils.mkdir_p(destination_dir, :verbose => true)
+          FileUtils.cp_r(file_path, File.join(destination_dir, File.basename(file).prepend(".")), :verbose => true)
+        else
+          full_destination_folder = File.join(destination_dir, dir_name.prepend("."))
+          FileUtils.mkdir_p(full_destination_folder, :verbose => true)
+          FileUtils.cp_r(file_path, File.join(full_destination_folder, File.basename(file)), :verbose => true)
         end
+      end
     end
+  end
 end
---
+++ vim/indent/ruby.vim
@@ -0,0 +1,9 @@
+" Vim plugin file
+" Language:	Ruby
+" Maintainer:	Pierre FILSTROFF <pfilstroff@gmail.com>
+" Last Change:	2013 Jan 18
+
+""" Indentation 
+" 2 spaces tabs
+au FileType ruby setlocal tabstop=2
+au FileType ruby setlocal shiftwidth=2
