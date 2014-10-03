95a8f13e3064955dd6c38ba0662fe780ce2b4f13
Pierre FILSTROFF <pfilstroff@gmail.com>
2014-10-03 16:19:14 +0200

    merge fgl zsh and reaper zsh

--- .zsh_custom/reaper.zsh
+++ .zsh_custom/reaper.zsh
@@ -1,6 +1,18 @@
 # ZSH configuration file
 # Created by Pierre FILSTROFF
 
+#FGL
+export FGLDIR=/opt/fourjs/fglgws/current #FGLDIR
+export FGLASDIR=/opt/fourjs/gas/current #FGLASDIR
+export FGLGDCDIR=/opt/fourjs/gdc/current #FGLGDCDIR
+export PATH=$PATH:$FGLDIR/bin #Fourjs bin path
+
+#CLIENTQA
+export FGLQADIR=~/Fourjs/Projects/git/clientqa
+
+#RUBTOOLS
+export FJS_DISTRIB_FOLDER=/work/distrib
+
 # JAVA / ANDROID
 export JDK_HOME=/usr/lib/jvm/java-8-oracle #JDKHOME
 export ANDROID=/opt/local/tools/64bits/android #ANDROID
@@ -14,7 +26,16 @@ export PATH=/opt/local/tools/64bits/qtcreator/bin:/opt/local/tools/64bits/qt/cur
 # GENYMOTION
 export PATH=/opt/local/tools/64bits/genymotion:$PATH
 
-# ALIASES
+#ALIASES
+#FGL
+alias gdc='$FGLGDCDIR/bin/gdc -aD'
+alias fglform='fglform -M'
+alias fglcomp='fglcomp -M'
+
+#COMMON
+alias route_to_local='sudo route add -net 10.10.0.0/24 gw 10.0.0.254 dev eth0'
+
+#ANDROID
 alias studio="screen -mdS AndroidStudio bash -c '$ANDROID_STUDIO/bin/studio.sh'"
 
 # Include reaper zsh plugins
