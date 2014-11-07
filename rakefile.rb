51fe7f0ec6bc53054c2a50a03895cecce344f381
Pierre FILSTROFF <pfilstroff@gmail.com>
2014-11-07 23:58:17 +0100

    Updated rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -6,8 +6,11 @@ end
 
 desc "Prepare dotfiles"
 task :prepare_dotfiles do
+  puts "\nUpdating submodules"
   submodule_up_cmd = "git submodule update --init"
   sh submodule_up_cmd
+
+  puts "\nUpdating plugins submodules"
   for plugin in Dir.glob File.join(".janus", "*")
     sh "cd #{plugin} && #{submodule_up_cmd}"
   end
@@ -15,6 +18,7 @@ end
 
 desc "Prepare home folder"
 task :prepare_home do
+  puts "\nRemove dotfiles symbolik links"
   for dotfile in dotfiles
     dotfile_expanded_path = File.join File.expand_path("~"), dotfile
     rm_r dotfile_expanded_path, verbose: true if File.exists? dotfile_expanded_path
@@ -23,11 +27,13 @@ end
 
 desc "Boostrap janus"
 task :bootstrap_janus do
+  puts "\nInitializing janus"
   sh "cd .vim && git checkout master && git pull && rake"
 end
 
 desc "Make symlinks of all files into home folder"
 task :make_symlinks do
+  puts "\nCreating symbolik links"
   for dotfile in dotfiles - janus_dotfiles_exclude
     dotfile_expanded_path = File.expand_path(dotfile)
     dest_dotfile_expanded_path = File.join File.expand_path("~"), dotfile
