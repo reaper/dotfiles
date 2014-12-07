4a8e88d386865e83cd315bcdef034ed947e4c635
Pierre FILSTROFF <pfilstroff@gmail.com>
2014-12-07 21:48:25 +0100

    Used reaper qt wombat fork and updated script to make symlink to the qt folder

--- .zsh_custom/reaper.zsh
+++ .zsh_custom/reaper.zsh
@@ -15,16 +15,19 @@ export FJS_DISTRIB_FOLDER=/work/distrib
 
 # JAVA / ANDROID
 export JDK_HOME=/usr/lib/jvm/java-8-oracle #JDKHOME
-export ANDROID=/opt/local/tools/64bits/android #ANDROID
+export ANDROID=/usr/local/tools/64bits/android #ANDROID
 export ANDROID_STUDIO=$ANDROID/studio #ANDROID STUDIO
 export ANDROID_HOME=$ANDROID/sdk #ANDROID HOME
 export ANDROID_NDK_HOME=$ANDROID/ndk/current #ANDROID HOME
 export PATH=${ANDROID_NDK_HOME}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
 
 # QT
-export PATH=/opt/local/tools/64bits/qtcreator/bin:/opt/local/tools/64bits/qt/current/bin:$PATH
+export QTCREATOR_HOME=/usr/local/tools/64bits/qtcreator/current
+export QTDIR=/usr/local/tools/64bits/qt/current
+export PATH=$QTCREATOR_HOME/bin:$QTDIR/bin:$PATH
+
 # GENYMOTION
-export PATH=/opt/local/tools/64bits/genymotion:$PATH
+export PATH=/usr/local/tools/64bits/genymotion:$PATH
 
 #ALIASES
 #FGL
@@ -37,6 +40,9 @@ alias route_to_local='sudo route add -net 10.10.0.0/24 gw 10.0.0.254 dev eth0'
 
 #ANDROID
 alias studio="screen -mdS AndroidStudio bash -c '$ANDROID_STUDIO/bin/studio.sh'"
+
+#QT
+alias qtcreator="qtcreator -stylesheet $HOME/.tools/themes/qt/wombat-theme/stylesheet.css"
 
 # Include reaper zsh plugins
 source $ZSH_CUSTOM/reaper/*.zsh
--- rakefile.rb
+++ rakefile.rb
@@ -1,12 +1,17 @@
 janus_dotfiles_exclude = %w(.vimrc .gvimrc)
-dotfiles = %w(.janus .vim .vimrc .gvimrc .vimrc.after .vimrc.before .gvimrc.after .gitconfig .gitignore_global .oh-my-zsh .zshrc .zsh_custom)
+dotfiles = %w(.janus .vim .vimrc .gvimrc .vimrc.after .vimrc.before .gvimrc.after .gitconfig .gitignore_global .oh-my-zsh .zshrc .zsh_custom .tools)
+
+def file_exists_or_symlink path
+  File.exists?(path) || File.symlink?(path)
+end
 
 task :init => [:prepare_dotfiles, :prepare_home, :bootstrap_janus, :make_symlinks] do
+  puts "Done"
 end
 
 desc "Prepare dotfiles"
 task :prepare_dotfiles do
-  puts "\nUpdating submodules"
+  puts "\nUpdate submodules"
   submodule_up_cmd = "git submodule update --init"
   sh submodule_up_cmd
 
@@ -18,11 +23,18 @@ end
 
 desc "Prepare home folder"
 task :prepare_home do
+  home_path = File.expand_path("~")
+
   puts "\nRemove dotfiles symbolik links"
   for dotfile in dotfiles
-    dotfile_expanded_path = File.join File.expand_path("~"), dotfile
-    rm_r dotfile_expanded_path, verbose: true if File.exists? dotfile_expanded_path
+    dotfile_expanded_path = File.join home_path, dotfile
+    rm_r dotfile_expanded_path, verbose: true if file_exists_or_symlink(dotfile_expanded_path)
   end
+
+  puts "\nRemove application symbolik links"
+  #QT wombat colorscheme
+  wombat_colorscheme_path = File.join home_path, ".config", "QtProject", "qtcreator", "styles", "wombat-style.xml"
+  rm_r wombat_colorscheme_path, verbose: true if file_exists_or_symlink(wombat_colorscheme_path)
 end
 
 desc "Boostrap janus"
@@ -33,12 +45,21 @@ end
 
 desc "Make symlinks of all files into home folder"
 task :make_symlinks do
-  puts "\nCreating symbolik links"
+  home_path = File.expand_path("~")
+
+  puts "\nCreate home symbolik links"
   for dotfile in dotfiles - janus_dotfiles_exclude
     dotfile_expanded_path = File.expand_path(dotfile)
-    dest_dotfile_expanded_path = File.join File.expand_path("~"), dotfile
+    dest_dotfile_expanded_path = File.join home_path, dotfile
     ln_s dotfile_expanded_path, dest_dotfile_expanded_path, verbose: true
   end
+
+  puts "\nCreate applications symbolik links"
+  #QT wombat colorscheme
+  tools_wombat_colorscheme_path = File.join home_path, ".tools", "themes", "qt", "wombat-theme", "wombat-style.xml"
+  wombat_colorscheme_path = File.join home_path, ".config", "QtProject", "qtcreator", "styles", "wombat-style.xml"
+  mkdir_p File.dirname(wombat_colorscheme_path), verbose: true unless File.exists?(File.dirname(wombat_colorscheme_path))
+  ln_s tools_wombat_colorscheme_path, wombat_colorscheme_path, verbose: true
 end
 
 task :default => :init
