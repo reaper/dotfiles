c0d2d87d66defd25b0c3aa83a39220dcc394e1de
Pierre FILSTROFF <pfilstroff@gmail.com>
2014-12-10 12:27:45 +0100

    Updated rakefile to be able to symlink qtcreator styles xml files

--- rakefile.rb
+++ rakefile.rb
@@ -30,11 +30,6 @@ task :prepare_home do
     dotfile_expanded_path = File.join home_path, dotfile
     rm_r dotfile_expanded_path, verbose: true if file_exists_or_symlink(dotfile_expanded_path)
   end
-
-  puts "\nRemove application symbolik links"
-  #QT wombat colorscheme
-  wombat_colorscheme_path = File.join home_path, ".config", "QtProject", "qtcreator", "styles", "wombat-style.xml"
-  rm_r wombat_colorscheme_path, verbose: true if file_exists_or_symlink(wombat_colorscheme_path)
 end
 
 desc "Boostrap janus"
@@ -55,11 +50,24 @@ task :make_symlinks do
   end
 
   puts "\nCreate applications symbolik links"
-  #QT wombat colorscheme
-  tools_wombat_colorscheme_path = File.join home_path, ".tools", "themes", "qt", "wombat-theme", "wombat-style.xml"
-  wombat_colorscheme_path = File.join home_path, ".config", "QtProject", "qtcreator", "styles", "wombat-style.xml"
-  mkdir_p File.dirname(wombat_colorscheme_path), verbose: true unless File.exists?(File.dirname(wombat_colorscheme_path))
+  tools_path = File.join home_path, ".tools"
+  styles_path = File.join home_path, ".config", "QtProject", "qtcreator", "styles"
+  mkdir_p styles_path, verbose: true unless File.exists?(styles_path)
+
+  #QTCreator wombat colorscheme
+  tools_wombat_colorscheme_path = File.join tools_path, "themes", "qt", "wombat-theme", "wombat-style.xml"
+  wombat_colorscheme_path = File.join styles_path, "wombat-style.xml"
+
+  rm_r wombat_colorscheme_path, verbose: true if file_exists_or_symlink(wombat_colorscheme_path)
   ln_s tools_wombat_colorscheme_path, wombat_colorscheme_path, verbose: true
+
+  #QTCreator themes
+  for theme_file_path in Dir.glob(File.join(tools_path, "themes", "qt", "qtcreator-themes", "*.xml"))
+    style_file_path = File.join styles_path, File.basename(theme_file_path)
+
+    rm_r style_file_path, verbose: true if file_exists_or_symlink(style_file_path)
+    ln_s theme_file_path, style_file_path, verbose: true
+  end
 end
 
 task :default => :init
