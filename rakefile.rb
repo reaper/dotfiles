f6f6265b1ec7956cd730db2fb274b2dd9e9c8438
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2021-10-18 11:16:59 +0200

    Added coc-settings + p10k.zsh

--- rakefile.rb
+++ rakefile.rb
@@ -1,55 +1,61 @@
-@dotfiles = %w(.gitconfig .gitignore_global .oh-my-zsh .zshrc .zsh_custom .dotfiles)
+@dotfiles = %w[.gitconfig .gitignore_global .oh-my-zsh .zshrc .zsh_custom .dotfiles]
 
-def file_exists_or_symlink path
-  File.exists?(path) || File.symlink?(path)
+def file_exists_or_symlink(path)
+  File.exist?(path) || File.symlink?(path)
 end
 
-task :init => [:prepare_dotfiles, :prepare_home, :make_symlinks] do
-  puts "Done"
+task init: %i[prepare_dotfiles prepare_home make_symlinks] do
+  puts 'Done'
 end
 
-desc "Prepare dotfiles"
+desc 'Prepare dotfiles'
 task :prepare_dotfiles do
   puts "\nUpdate submodules"
-  sh "git submodule update --init --remote"
+  sh 'git submodule update --init --remote'
 
   # Install vim-plug
   sh "curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 end
 
-desc "Prepare home folder"
+desc 'Prepare home folder'
 task :prepare_home do
-  home_path = File.expand_path("~")
+  home_path = File.expand_path('~')
 
   puts "\nRemove dotfiles symbolik links"
-  for dotfile in @dotfiles
+  @dotfiles.each do |dotfile|
     dotfile_expanded_path = File.join home_path, dotfile
     rm_r dotfile_expanded_path, verbose: true if file_exists_or_symlink(dotfile_expanded_path)
   end
 end
 
-desc "Make symlinks of all files into home folder"
+desc 'Make symlinks of all files into home folder'
 task :make_symlinks do
-  home_path = File.expand_path("~")
+  home_path = File.expand_path('~')
 
   puts "\nCreate home symbolik links"
-  for dotfile in @dotfiles
+  @dotfiles.each do |dotfile|
     dotfile_expanded_path = File.expand_path(dotfile)
     dest_dotfile_expanded_path = File.join home_path, dotfile
     ln_s dotfile_expanded_path, dest_dotfile_expanded_path, verbose: true
   end
 
-  dotfiles_home_path = File.join home_path, ".dotfiles"
+  dotfiles_home_path = File.join home_path, '.dotfiles'
 
   puts "\nCreate applications symbolik links"
 
-  nvim_init_path = File.join home_path, ".config", "nvim", "init.lua"
-  dotfiles_nvim_init_path = File.join dotfiles_home_path, "config", "nvim", "init.lua"
+  %w[
+    .config/nvim/init.lua
+    .config/nvim/coc-settings.json
+    .p10k.zsh
+  ].each do |file|
+    file_path = File.join(home_path, *file.split('/'))
+    dotfiles_file_path = File.join(dotfiles_home_path, *file.delete_prefix('.').split('/'))
 
-  rm_r nvim_init_path , verbose: true if file_exists_or_symlink(nvim_init_path)
-  mkdir_p File.dirname(nvim_init_path) unless File.directory?(nvim_init_path)
-  ln_s dotfiles_nvim_init_path, nvim_init_path, verbose: true
+    rm_r file_path, verbose: true if file_exists_or_symlink(file_path)
+    mkdir_p(File.dirname(file_path)) unless File.exist?(file_path)
+    ln_s dotfiles_file_path, file_path, verbose: true
+  end
 end
 
-task :default => :init
+task default: :init
