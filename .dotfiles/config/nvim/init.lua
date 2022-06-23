9f50e2f16669a9ef92b011b72c3287f280eb8685
Pierre FILSTROFF <pierre.filstroff@gmail.com>
2022-06-23 19:52:16 +0200

    Small change

--- .dotfiles/config/nvim/init.lua
+++ .dotfiles/config/nvim/init.lua
@@ -196,7 +196,7 @@ local saga = require("lspsaga")
 saga.init_lsp_saga({
   code_action_icon = " ",
   definition_preview_icon = "  ",
-  dianostic_header_icon = "   ",
+  -- dianostic_header_icon = "   ",
   error_sign = " ",
   finder_definition_icon = "  ",
   finder_reference_icon = "  ",
